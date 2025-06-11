5e307eea340058d799db7bb2d0e4268ccdac32d6
Balia-KaoKhun <kaokhunid2@gmail.com>
2025-06-11 10:04:17 +0700

    ส่งใบงาน 2
    ใบงานนี้เป็นการฝึกใช้คำสั่งพื้นฐานของ Python ได้แก่ print(), input(), len(), type() และการ debug error

---
+++ Work/01-name_length.py
@@ -0,0 +1,4 @@
+name = input("what's ur firstname?")
+sur = input("what's ur surname?")
+print(len(name))
+print(len(sur))
---
+++ Work/02-word_concat.py
@@ -0,0 +1,4 @@
+fist = input("fist word ")
+sec = input("second word ")
+
+print(f"{fist} + {sec}")
